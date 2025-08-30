689004e57523afae6f57a6a81705a3cfb301a4bf
Shiva-Ch0wdary <shivachowdary753@gmail.com>
2025-08-30 12:35:51 +0530

    Updated cros issues

--- backend/server.js
+++ backend/server.js
@@ -67,6 +67,8 @@ app.use(
       "http://localhost:8080",
       "http://localhost:8081",
       "http://localhost:3000",
+      "https://triveshatech.vercel.app",
+      "https://trivesha.tech",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
